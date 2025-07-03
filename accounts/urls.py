617dbe4f9695af1e468986fdcb1399f45338b75a
NoahDD-01 <noahdd216@gmail.com>
2025-07-03 21:14:50 +0630

    commit3

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,17 @@
+from django.contrib import admin
 from django.urls import path
 from accounts.views import CustomTokenObtainPairView
 from rest_framework_simplejwt.views import TokenRefreshView
 
+from .views import *
+
 urlpatterns = [
     path("auth/login/", CustomTokenObtainPairView.as_view(), name ="token_obtain_pair"),
-    path("auth/refresh/", TokenRefreshView.as_view(),name="token_refresh")
+    path("auth/refresh/", TokenRefreshView.as_view(),name="token_refresh"), 
     
+    path("register/",register_user,name="register"),
+    path('users/',user_list,name='user-list'),
+    path('users/<uuid:pk>/',user_detail,name='user-detail'),
+    path('users/<uuid:pk>/update/',user_update,name='user-update'),
+    path('users/<uuid:pk>/delete/',user_delete,name='user-delete'),
 ]
